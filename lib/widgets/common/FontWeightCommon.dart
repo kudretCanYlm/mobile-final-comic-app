b4dd88e3730f34aebe0ef127ab6d28185ab82a2b
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2023-01-03 09:31:26 +0300

    refactored

--- lib/widgets/common/FontWeightCommon.dart
+++ lib/widgets/common/FontWeightCommon.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 const FontWeight FONT_WEIGHT_1 = FontWeight.w100;
 const FontWeight FONT_WEIGHT_2 = FontWeight.w200;
 const FontWeight FONT_WEIGHT_3 = FontWeight.w300;
-const FontWeight FONT_WEIGHT_4 = FontWeight.w500;
+const FontWeight FONT_WEIGHT_4 = FontWeight.w400;
 const FontWeight FONT_WEIGHT_5 = FontWeight.w500;
 const FontWeight FONT_WEIGHT_6 = FontWeight.w600;
 const FontWeight FONT_WEIGHT_7 = FontWeight.w700;
