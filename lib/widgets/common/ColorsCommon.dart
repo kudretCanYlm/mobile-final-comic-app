b96ad54892d43d578291da0f1e28a4cbe8e82f2c
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2023-01-03 09:30:51 +0300

    added new things

--- lib/widgets/common/ColorsCommon.dart
+++ lib/widgets/common/ColorsCommon.dart
@@ -5,6 +5,8 @@ const Color COLOR_A_LIGHT = Color.fromRGBO(38, 154, 242, 1);
 
 const Color COLOR_B_HEAVY = Color.fromRGBO(231, 8, 142, 1);
 const Color COLOR_B_LIGHT = Color.fromRGBO(247, 43, 166, 1);
+const Color COLOR_B_LIGHT_2 = Color.fromARGB(255, 255, 96, 191);
+const Color COLOR_B_LIGHT_3 = Color.fromARGB(255, 255, 136, 207);
 
 const Color COLOR_C_HEAVY = Color.fromARGB(255, 234, 125, 70);
 const Color COLOR_C_LIGHT = Color.fromRGBO(237, 139, 90, 1);
@@ -14,6 +16,8 @@ const Color COLOR_D_LIGHT = Color.fromARGB(255, 55, 55, 55);
 const Color COLOR_D_LIGHT_2 = Color.fromARGB(255, 100, 100, 100);
 
 const Color COLOR_E_HEAVY = Color.fromRGBO(240, 240, 240, 1);
+const Color COLOR_E_HEAVY_2 = Color.fromARGB(255, 255, 255, 255);
 const Color COLOR_E_LIGHT = Color.fromRGBO(224, 224, 224, 1);
 
 const Color COLOR_F_LOS = Color.fromARGB(255, 178, 178, 178);
+const Color COLOR_F_HEAVY = Color.fromARGB(255, 208, 208, 208);
