859e8b908b30db97aae77d1c562e5e1c3ec06f8c
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2023-03-06 19:08:05 +0300

    added json serializable

--- lib/models/Comic/FavoriteModel.dart
+++ lib/models/Comic/FavoriteModel.dart
@@ -1,7 +1,16 @@
+import 'package:json_annotation/json_annotation.dart';
+part 'FavoriteModel.g.dart';
+
+@JsonSerializable()
 class FavoriteModel {
   String Id;
   String Name;
   int Icon;
 
   FavoriteModel(this.Id, this.Name, this.Icon);
+
+  factory FavoriteModel.fromJson(Map<String, dynamic> json) =>
+      _$FavoriteModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$FavoriteModelToJson(this);
 }
--- lib/models/Search/SearchResultModel.dart
+++ lib/models/Search/SearchResultModel.dart
@@ -1,5 +1,8 @@
 import 'package:comic_mobile_app/models/Search/common/SearchResultTypes.dart';
+import 'package:json_annotation/json_annotation.dart';
+part 'SearchResultModel.g.dart';
 
+@JsonSerializable()
 class SearchResultModel {
   final SearchResultTypes ResultType;
   final String Id;
@@ -7,4 +10,9 @@ class SearchResultModel {
   final String Name;
 
   SearchResultModel(this.ResultType, this.Id, this.Base64Image, this.Name);
+
+  factory SearchResultModel.fromJson(Map<String, dynamic> json) =>
+      _$SearchResultModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$SearchResultModelToJson(this);
 }
